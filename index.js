0b1d7c933011f9b7eec67ac024c1b81d7d86a472
Matthew Stone <mattstone@google.com>
2015-08-04 16:10:42 -0700

    Start work on mapping visualizer

--- index.js
+++ index.js
@@ -17,6 +17,7 @@ app.get('/', function(req, res){
 
 app.use('/bower_components', express.static(__dirname + '/bower_components'));
 app.use('/js', express.static(__dirname + '/js'));
+app.use('/css', express.static(__dirname + '/css'));
 
 
 io.on('connection', function(socket){
--- js/controller.js
+++ js/controller.js
@@ -33,6 +33,15 @@ app.controller('IndexCtrl', function ($scope, mySocket) {
     mySocket.emit('get mapping');
   };
 
+  $scope.getStyle = function(center) {
+    console.log(center);
+    var w = 800;
+    var h = 600;
+    var x = (center[0] * (w/2)) + w/2;
+    var y = (center[1] * (h/2)) + h/2;
+    console.log(x,y);
+  }
+
   $scope.pushers = {};
   $scope.getPushers();
   $scope.getMapping();
