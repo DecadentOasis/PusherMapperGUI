9dad0fecf692a802ed1eea8008ece664105092c4
robot-head <matthew.d.stone@gmail.com>
2015-08-05 22:54:31 -0700

    Add material design and coordinate saving

--- index.js
+++ index.js
@@ -25,11 +25,17 @@ io.on('connection', function (socket) {
     socket.on('disconnect', function () {
         console.log('user disconnected');
     });
+
     socket.on('pusher select', function (mac_addr) {
         console.log('Pusher Selected, MAC: ' + mac_addr);
     });
-    socket.on('save tree', function (mac_addr, type, x, y) {
-        console.log('Tree save requested. MAC: %s, type: %s, (%s, %s)', mac_addr, type, x, y);
+
+    socket.on('save tree', function (mac_addr, strip_no, type, x, y) {
+        console.log('Tree save requested. MAC: %s, Strip#: %s, type: %s, (%s, %s)', mac_addr, strip_no, type, x, y);
+        var fixture = mapping[mac_addr][strip_no];
+        fixture.type = type;
+        fixture.center = [x, y];
+        writeMappingToDisk();
         console.log('Tree saved!');
     });
 
@@ -40,6 +46,7 @@ io.on('connection', function (socket) {
         console.log('list pushers called');
         socket.emit('pushers', pushers);
     });
+
     socket.on('get mapping', function () {
         console.log('get mapping called');
         socket.emit('pusher mapping', mapping);
@@ -50,8 +57,10 @@ http.listen(3000, function () {
     console.log('listening on *:3000');
 });
 
+var mappingFilename = 'mapping.json';
+
 var readMappingFromDisk = function () {
-    fs.readFile('mapping.json', 'utf8', function (err, data) {
+    fs.readFile(mappingFilename, 'utf8', function (err, data) {
         if (err) {
             console.log(err);
             return;
@@ -59,6 +68,17 @@ var readMappingFromDisk = function () {
         mapping = JSON.parse(data);
         console.log('Read mapping JSON:');
         console.log(mapping);
+    });
+}
+
+var writeMappingToDisk = function () {
+
+    fs.writeFile(mappingFilename, JSON.stringify(mapping, null, 4), function(err) {
+        if(err) {
+            console.log(err);
+        } else {
+            console.log("JSON saved to " + mappingFilename);
+        }
     });
 }
 
--- js/controller.js
+++ js/controller.js
@@ -1,6 +1,7 @@
 var app = angular.module('PusherMapperGUI', [
     'btford.socket-io',
-    'ngDragDrop'
+    'ngDragDrop',
+    'ngMaterial',
 ]);
 app.factory('mySocket', function (socketFactory) {
     var mySocket = socketFactory();
@@ -37,23 +38,36 @@ app.controller('IndexCtrl', function ($scope, mySocket) {
         mySocket.emit('get mapping');
     };
 
+    $scope.getForestWidth = function () {
+        return document.getElementById('forest_preview').clientWidth;
+    };
+
+    $scope.getForestHeight = function () {
+        return document.getElementById('forest_preview').clientHeight;
+    };
+
     $scope.stopCallback = function (event, ui) {
-        var w = $scope.viewport_w;
-        var h = $scope.viewport_h;
+        var w = $scope.getForestWidth();
+        var h = $scope.getForestHeight();;
         var l = ui.position.left;
         var t = ui.position.top;
-        var x = (l - (w / 2)) / (w / 2);
-        var y = (t - (h / 2)) / (h / 2);
+        var x = Math.round((l - (w / 2)) / (w / 2) * 100) / 100;
+        var y = Math.round((t - (h / 2)) / (h / 2) * 100) / 100;
         var mac_addr = this.$parent.key;
         var fixture_type = this.fixture.type;
+        this.fixture.center[0] = x;
+        this.fixture.center[1] = y;
+        var strip_no = this.$index;
         console.log('%d,%d: %f,%f', ui.offset.left, ui.offset.top, x, y);
-        mySocket.emit('save tree', mac_addr, fixture_type, x, y);
+        console.log(this.fixture);
+        mySocket.emit('save tree', mac_addr, strip_no, fixture_type, x, y);
+        $scope.$apply();
     };
 
     $scope.getStyle = function (center) {
 
-        var w = $scope.viewport_w;
-        var h = $scope.viewport_h;
+        var w = $scope.getForestWidth();
+        var h = $scope.getForestHeight();;
         var x = (center[0] * (w / 2)) + w / 2;
         var y = (center[1] * (h / 2)) + h / 2;
         return {'top': y + 'px', 'left': x + 'px'};
