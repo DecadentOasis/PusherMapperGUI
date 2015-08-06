6ee10b8524ef65a6425539a192a86900fc35c243
robot-head <matthew.d.stone@gmail.com>
2015-08-05 21:31:51 -0700

    Make drag and drop work!

--- index.js
+++ index.js
@@ -5,14 +5,14 @@ var io = require('socket.io')(http);
 var fs = require('fs');
 
 var pushers = {
-  '95E33CC97C0D': {},
-  'DCC54C3705C1': {}
+    '95E33CC97C0D': {},
+    'DCC54C3705C1': {}
 };
 
 var mapping;
 
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/index.html');
 });
 
 app.use('/bower_components', express.static(__dirname + '/bower_components'));
@@ -20,41 +20,46 @@ app.use('/js', express.static(__dirname + '/js'));
 app.use('/css', express.static(__dirname + '/css'));
 
 
-io.on('connection', function(socket){
-  console.log('a user connected');
-  socket.on('disconnect', function() {
-    console.log('user disconnected');
-  });
-  socket.on('pusher select', function(mac_addr) {
-    console.log('Pusher Selected, MAC: ' + mac_addr);
-  });
-  socket.on('list pushers', function() {
-    Object.keys(pushers).forEach(function(key) {
-      pushers[key].timestamp = (new Date);
-    })
-    console.log('list pushers called');
-    socket.emit('pushers', pushers);
-  });
-  socket.on('get mapping', function() {
-    console.log('get mapping called');
-    socket.emit('pusher mapping', mapping);
-  });
+io.on('connection', function (socket) {
+    console.log('a user connected');
+    socket.on('disconnect', function () {
+        console.log('user disconnected');
+    });
+    socket.on('pusher select', function (mac_addr) {
+        console.log('Pusher Selected, MAC: ' + mac_addr);
+    });
+    socket.on('save tree', function (mac_addr, type, x, y) {
+        console.log('Tree save requested. MAC: %s, type: %s, (%s, %s)', mac_addr, type, x, y);
+        console.log('Tree saved!');
+    });
+
+    socket.on('list pushers', function () {
+        Object.keys(pushers).forEach(function (key) {
+            pushers[key].timestamp = (new Date);
+        })
+        console.log('list pushers called');
+        socket.emit('pushers', pushers);
+    });
+    socket.on('get mapping', function () {
+        console.log('get mapping called');
+        socket.emit('pusher mapping', mapping);
+    });
 });
 
-http.listen(3000, function(){
-  console.log('listening on *:3000');
+http.listen(3000, function () {
+    console.log('listening on *:3000');
 });
 
-var readMappingFromDisk = function() {
-  fs.readFile('mapping.json', 'utf8', function (err, data) {
-    if (err) {
-      console.log(err);
-      return;
-    }
-    mapping = JSON.parse(data);
-    console.log('Read mapping JSON:');
-    console.log(mapping);
-  });
+var readMappingFromDisk = function () {
+    fs.readFile('mapping.json', 'utf8', function (err, data) {
+        if (err) {
+            console.log(err);
+            return;
+        }
+        mapping = JSON.parse(data);
+        console.log('Read mapping JSON:');
+        console.log(mapping);
+    });
 }
 
 readMappingFromDisk();
--- js/controller.js
+++ js/controller.js
@@ -1,48 +1,66 @@
 var app = angular.module('PusherMapperGUI', [
-  'btford.socket-io'
+    'btford.socket-io',
+    'ngDragDrop'
 ]);
 app.factory('mySocket', function (socketFactory) {
-  var mySocket = socketFactory();
-  mySocket.forward('error');
-  mySocket.forward('pushers');
-  mySocket.forward('pusher mapping');
-  return mySocket;
+    var mySocket = socketFactory();
+    mySocket.forward('error');
+    mySocket.forward('pushers');
+    mySocket.forward('pusher mapping');
+    return mySocket;
 });
 
 app.controller('IndexCtrl', function ($scope, mySocket) {
 
-  $scope.$on('socket:error', function (ev, data) {
-    console.log(data);
-  });
-  $scope.$on('socket:pushers', function(ev, data) {
-    angular.forEach(data, function(value, key) {
-      $scope.pushers[key] = value;
+    $scope.viewport_w = 800;
+    $scope.viewport_h = 600;
+
+    $scope.$on('socket:error', function (ev, data) {
+        console.log(data);
     });
-    $scope.pushers = data;
-  });
+    $scope.$on('socket:pushers', function (ev, data) {
+        angular.forEach(data, function (value, key) {
+            $scope.pushers[key] = value;
+        });
+        $scope.pushers = data;
+    });
 
-  $scope.$on('socket:pusher mapping', function(ev, data) {
-    $scope.mapping = data;
-  });
+    $scope.$on('socket:pusher mapping', function (ev, data) {
+        $scope.mapping = data;
+    });
 
-  $scope.getPushers = function() {
-    mySocket.emit('list pushers');
-  };
+    $scope.getPushers = function () {
+        mySocket.emit('list pushers');
+    };
 
-  $scope.getMapping = function() {
-    mySocket.emit('get mapping');
-  };
+    $scope.getMapping = function () {
+        mySocket.emit('get mapping');
+    };
 
-  $scope.getStyle = function(center) {
+    $scope.stopCallback = function (event, ui) {
+        var w = $scope.viewport_w;
+        var h = $scope.viewport_h;
+        var l = ui.position.left;
+        var t = ui.position.top;
+        var x = (l - (w / 2)) / (w / 2);
+        var y = (t - (h / 2)) / (h / 2);
+        var mac_addr = this.$parent.key;
+        var fixture_type = this.fixture.type;
+        console.log('%d,%d: %f,%f', ui.offset.left, ui.offset.top, x, y);
+        mySocket.emit('save tree', mac_addr, fixture_type, x, y);
+    };
 
-    var w = 800;
-    var h = 600;
-    var x = (center[0] * (w/2)) + w/2;
-    var y = (center[1] * (h/2)) + h/2;
-    return {'top': y + 'px', 'left': x + 'px'};
-  }
+    $scope.getStyle = function (center) {
 
-  $scope.pushers = {};
-  $scope.getPushers();
-  $scope.getMapping();
+        var w = $scope.viewport_w;
+        var h = $scope.viewport_h;
+        var x = (center[0] * (w / 2)) + w / 2;
+        var y = (center[1] * (h / 2)) + h / 2;
+        return {'top': y + 'px', 'left': x + 'px'};
+    }
+
+    $scope.pushers = {};
+    $scope.getPushers();
+    $scope.getMapping();
+
 });
