f9baaf687e4fb26da1359088010c0e2e5841e998
Matt Stone <matthew.d.stone@gmail.com>
2015-08-08 17:14:56 -0700

    Canvas

--- js/controller.js
+++ js/controller.js
@@ -13,9 +13,6 @@ app.factory('mySocket', function (socketFactory) {
 
 app.controller('IndexCtrl', function ($scope, mySocket) {
 
-    $scope.viewport_w = 800;
-    $scope.viewport_h = 600;
-
     $scope.$on('socket:error', function (ev, data) {
         console.log(data);
     });
@@ -24,10 +21,28 @@ app.controller('IndexCtrl', function ($scope, mySocket) {
             $scope.pushers[key] = value;
         });
         $scope.pushers = data;
+
     });
 
     $scope.$on('socket:pusher mapping', function (ev, data) {
         $scope.mapping = data;
+        var canvas = document.getElementById('forest-preview');
+        if (canvas.getContext){
+            var ctx = canvas.getContext('2d');
+            ctx.canvas.width  = window.innerWidth;
+            ctx.canvas.height = window.innerHeight;
+            ctx.fillStyle = "rgb(200,0,0)";
+            angular.forEach(data, function(value, key) {
+                var pusher = value;
+                for (var i = pusher.length - 1; i >= 0; i--) {
+                    var coords = getRelativeCoords(pusher[i].center, ctx);
+                    var x = coords[0];
+                    var y = coords[1];
+                    var size = 20;
+                    ctx.fillRect(x-size, y-size, size, size);
+                };
+            });
+        }
     });
 
     $scope.getPushers = function () {
@@ -64,6 +79,14 @@ app.controller('IndexCtrl', function ($scope, mySocket) {
         $scope.$apply();
     };
 
+    var getRelativeCoords = function(coords, ctx) {
+        var w = ctx.canvas.width;
+        var h = ctx.canvas.height;
+        var x = (coords[0] * (w / 2)) + w / 2;
+        var y = (coords[1] * (h / 2)) + h / 2;
+        return [x, y];
+    };
+
     $scope.getStyle = function (center) {
 
         var w = $scope.getForestWidth();
@@ -71,7 +94,8 @@ app.controller('IndexCtrl', function ($scope, mySocket) {
         var x = (center[0] * (w / 2)) + w / 2;
         var y = (center[1] * (h / 2)) + h / 2;
         return {'top': y + 'px', 'left': x + 'px'};
-    }
+    };
+
 
     $scope.pushers = {};
     $scope.getPushers();
